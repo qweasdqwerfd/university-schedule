f9c9011f53735bf0559d3b0db9953b6c3a794ad6
bdsm <36733200+sTePmImIMi@users.noreply.github.com>
2025-08-28 15:56:33 +0300

    created TaskDetailsScreen darkTheme

--- app/src/main/java/com/example/universityschedule/ui/screens/tasks/TaskDetailsScreen.kt
+++ app/src/main/java/com/example/universityschedule/ui/screens/tasks/TaskDetailsScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -16,6 +18,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,6 +29,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.universityschedule.R
 import com.example.universityschedule.R.drawable
+import com.example.universityschedule.ui.custom_components.IconButton.def.DetailsButton
 import com.example.universityschedule.ui.custom_components.UniversityScheduleTheme
 
 @Preview
@@ -33,8 +37,265 @@ import com.example.universityschedule.ui.custom_components.UniversityScheduleThe
 fun TaskDetailsScreen() {
     UniversityScheduleTheme(
         darkTheme = true,
-        content = {}
-        )
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(
+                        horizontal = 20.dp,
+                        vertical = 20.dp
+                    ),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Essay Draft",
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                    Icon(
+                        painter = painterResource(drawable.circle),
+                        contentDescription = "circle or tick",
+                        modifier = Modifier
+                            .size(30.dp),
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .padding(top = 4.dp)
+                        .background(
+                            color = Color(0xFFB7960B),
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                        .padding(
+                            horizontal = 10.dp,
+                            vertical = 5.dp
+                        ),
+
+                    ) {
+                    Row(
+                        modifier = Modifier
+                            .width(120.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(drawable.warn),
+                            contentDescription = "ward",
+                            modifier = Modifier
+                                .size(13.dp),
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+
+
+                        Text(
+                            text = "Medium Priority",
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            fontWeight = FontWeight.Bold
+                        )
+
+                    }
+
+                }
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 27.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+                        contentColor = MaterialTheme.colorScheme.onSurface
+                    )
+                ) {
+
+                    Column(
+                        modifier = Modifier
+                            .padding(
+                                horizontal = 15.dp,
+                                vertical = 13.dp
+                            ),
+                    ) {
+                        Row {
+                            Icon(
+                                painter = painterResource(drawable.calendar),
+                                contentDescription = "calendar",
+                                Modifier
+                                    .size(18.dp)
+                                    .align(Alignment.CenterVertically),
+                                tint = MaterialTheme.colorScheme.onBackground
+                            )
+                            Spacer(Modifier.width(8.dp))
+                            Text(
+                                text = "Due Date",
+                                style = MaterialTheme.typography.titleMedium,
+                                color = MaterialTheme.colorScheme.onSurface,
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                        }
+
+                        Spacer(Modifier.height(15.dp))
+
+                        Text(
+                            text = "Wednesday, April 2 at 09:34 PM",
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 27.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+                        contentColor = MaterialTheme.colorScheme.onSurface
+                    )
+                ) {
+
+                    Column(
+                        modifier = Modifier
+                            .padding(
+                                horizontal = 15.dp,
+                                vertical = 13.dp
+                            ),
+                    ) {
+                        Row {
+                            Icon(
+                                painter = painterResource(drawable.lessons),
+                                contentDescription = "calendar",
+                                Modifier
+                                    .size(18.dp)
+                                    .align(Alignment.CenterVertically),
+                                tint = MaterialTheme.colorScheme.onBackground
+                            )
+                            Spacer(Modifier.width(8.dp))
+                            Text(
+                                text = "Related Lesson",
+                                style = MaterialTheme.typography.titleMedium,
+                                color = MaterialTheme.colorScheme.onSurface,
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                        }
+
+                        Spacer(Modifier.height(15.dp))
+
+                        Row {
+
+                            VerticalDivider(
+                                modifier = Modifier
+                                    .height(43.dp)
+                                    .width(16.dp),
+                                color = Color.Green,
+                                thickness = 3.dp
+                            )
+
+
+                            Column {
+
+                                Text(
+                                    text = "Literature",
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    color = MaterialTheme.colorScheme.onSurface
+                                )
+
+                                Spacer(Modifier.height(7.dp))
+
+                                Row {
+                                    Icon(
+                                        painter = painterResource(drawable.clock),
+                                        contentDescription = "clock",
+                                        modifier = Modifier
+                                            .size(12.dp)
+                                            .align(Alignment.CenterVertically),
+                                        tint = MaterialTheme.colorScheme.onBackground
+                                    )
+
+                                    Spacer(Modifier.width(7.dp))
+
+                                    Text(
+                                        text = "10:00 - 11:30",
+                                        style = MaterialTheme.typography.bodySmall,
+                                        color = MaterialTheme.colorScheme.onSurface,
+                                        modifier = Modifier
+                                            .align(Alignment.CenterVertically)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 27.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+                        contentColor = MaterialTheme.colorScheme.onSurface
+                    )
+                ) {
+
+                    Column(
+                        modifier = Modifier
+                            .padding(
+                                horizontal = 15.dp,
+                                vertical = 13.dp
+                            ),
+                    ) {
+                        Text(
+                            text = "Description",
+                            style = MaterialTheme.typography.titleMedium,
+                            color = MaterialTheme.colorScheme.onSurface,
+                        )
+
+                        Spacer(Modifier.height(15.dp))
+
+                        Text(
+                            text = "Write first draft of comparative essay",
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+
+                Spacer(Modifier.height(33.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(5.dp),
+                ) {
+                    DetailsButton(
+                        modifier = Modifier.weight(1f),
+                        text = "Edit Task",
+                        color = Color.Blue,
+                        icon = drawable.edit,
+                        sizeIcon = 25,
+                    )
+                    DetailsButton(
+                        modifier = Modifier.weight(1f),
+                        text = "Delete Task",
+                        color = Color.Red,
+                        icon = drawable.trash,
+                        sizeIcon = 21,
+                    )
+                }
+
+
+            }
+        }
+    )
 
 }
 
