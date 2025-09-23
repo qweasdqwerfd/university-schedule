09401ecd2cd1c4ddd54dbc633fa94ed0541448ef
bdsm <36733200+sTePmImIMi@users.noreply.github.com>
2025-09-23 14:55:56 +0300

    bug fix

--- app/src/main/java/com/example/universityschedule/presentation/navigation/Screen.kt
+++ app/src/main/java/com/example/universityschedule/presentation/navigation/Screen.kt
@@ -6,6 +6,6 @@ sealed class Screen(val route: String) {
     object TASKS : Screen("tasks")
     object ADD_TASK : Screen("add_task")
     object TASK_DETAILS: Screen("task_details/{taskId}") {
-        fun createRoute(taskId: Int) = "task_details/$taskId"
+        fun createRoute(taskId: Int) = "task_details/$taskId?ts=${System.currentTimeMillis()}"
     }
 }
